bb3e810696f1d90f9a78cca90047bc1eed6fa75f
Gervais Amoah <gervaisamoah@yahoo.com>
2023-05-21 11:29:30 +0000

    Go introduction

--- 12_api/code/api.go
+++ 12_api/code/api.go
@@ -1,12 +1,85 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
+
+// Planet is a planet
+type Planet struct {
+	Name       string `json:"name"`
+	Population string `json:"population"`
+	Terrain    string `json:"terrain"`
+}
+
+// Person is a person
+type Person struct {
+	Name         string `json:"name"`
+	HomeworldURL string `json:"homeworld"`
+	Homeworld    Planet
+}
+
+// AllPeople is a bunch of people
+type AllPeople struct {
+	People []Person `json:"results"`
+}
 
 // BaseURL is the base endpoint for the star wars API
 const BaseURL = "https://swapi.dev/api/"
 
+func (p *Person) getHomeWorld() {
+	res, err := http.Get(p.HomeworldURL)
+	if err != nil {
+		log.Println("Error fetching person's home world")
+	}
+
+	if dataBytes, err := ioutil.ReadAll(res.Body); err != nil {
+		log.Println("Error parsing res.body")
+	} else {
+		json.Unmarshal(dataBytes, &p.Homeworld)
+	}
+
+}
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Getting people")
+	res, err := http.Get(BaseURL + "people")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Failed to request star wars people")
+	}
+
+	fmt.Println(res)
+
+	dataBytes, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Failed to parse request body")
+	}
+
+	var people AllPeople
+
+	if err := json.Unmarshal(dataBytes, &people); err != nil {
+		fmt.Println("Error parsing json", err)
+	}
+
+	fmt.Println(people)
+
+	for _, person := range people.People {
+		person.getHomeWorld()
+		fmt.Println(person)
+	}
+
+}
+
 func main() {
-	fmt.Println(BaseURL)
+	//	create route for /people
+	http.HandleFunc("/people", getPeople)
+	fmt.Println("Serving at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- 13_concurrency/code/concurrency.go
+++ 13_concurrency/code/concurrency.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func say(s string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(s)
-		time.Sleep(time.Millisecond * 300)
+var wg sync.WaitGroup
+
+func handlePlanic() {
+	if r := recover(); r != nil {
+		fmt.Println("PANIC!")
 	}
 }
 
+func printStuff() {
+	defer wg.Done()
+	defer handlePlanic()
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+		time.Sleep(time.Millisecond * 300)
+	}
+
+}
+
 func main() {
-	say("Hello")
-	say("There")
+	wg.Add(1)
+	go printStuff()
+	wg.Wait()
 }
